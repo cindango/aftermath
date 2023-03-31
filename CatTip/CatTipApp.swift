1c94013bafc8fbbce75c1069e9bb290426af0d87
Cindy Ho <cindango@gmail.com>
2023-03-30 22:36:04 -0400

    Initial Commit

---
+++ CatTip/CatTipApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatTipApp.swift
+//  CatTip
+//
+//  Created by Cindy Ho on 3/30/23.
+//
+
+import SwiftUI
+
+@main
+struct CatTipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
